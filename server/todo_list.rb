a0c82d0803c705024cc03609454584968fd5bb70
Ilya Hrytsuk <bypotatoes@gmail.com>
2020-08-16 20:36:57 +0200

    Create basic server to emulate json api

--- server/todo_list.rb
+++ server/todo_list.rb
@@ -1,15 +1,35 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'sinatra/cors'
+require 'rom'
+require 'rom-repository'
+
+set :allow_origin, '*'
+set :allow_methods, 'GET,HEAD,POST'
+set :allow_headers, 'content-type,if-modified-since'
+set :expose_headers, 'location,link'
+
+rom = ROM.container(:sql, 'sqlite::memory') do |config|
+  config.default.connection.create_table(:todos) do
+    primary_key :id
+    column :title, String, null: false
+    column :description, String, null: false
+    column :due_date, Date, null: true
+  end
+
+  config.relation(:todos) do
+    schema(infer: true)
+  end
+end
+
+class TodoRepo < ROM::Repository[:todos]
+  def all
+    todos.to_a
+  end
+end
+
+todo_repo = TodoRepo.new(rom)
 
 get '/todos' do
-  todos = [
-    {
-      id: 1,
-      title: '42',
-      description: 'caramba',
-      due_date: Date.today
-    }
-  ]
-
-  json todos
+  json todo_repo.all
 end
